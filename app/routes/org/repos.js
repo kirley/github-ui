a590b572117997312412fc03190050d9b0127682
Aaron Kirley <kirley@gmail.com>
2016-06-17 21:49:47 -0400

    complete

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function() {
+    return [
+      {id: "calendar-ui", name: "Calendar UI"},
+      {id: "datepicker-ui", name: "Date Picker UI"},
+      {id: "accordion-ui", name: "Accordion UI"}
+    ];
+  }
+
 });
