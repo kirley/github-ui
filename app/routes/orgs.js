333493d1b1c9172f5c9f8dcd29ca8505f6110b2d
Aaron Kirley <kirley@gmail.com>
2016-06-17 23:38:50 -0400

    e3 done

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -2,6 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  favorites: Ember.inject.service(),
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('items', this.get('favorites.items'));
+  },
+
+  actions: {
+    addFavorite(value) {
+      // alert(`hi: ${value}`);
+      this.get('favorites.items').addObject({id: value});
+    }
+  },
+
   model: function() {
     return [
       {id: "emberjs"},
